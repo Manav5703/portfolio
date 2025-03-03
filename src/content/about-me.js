799d51cfe5a4b5b0f87430c040e6b91693f168a3
Manav <manavpatel5703.com>
2025-03-03 14:20:52 -0400

    fixed links

--- src/content/about-me.js
+++ src/content/about-me.js
@@ -6,7 +6,7 @@ export default [
   {
     input: 'Manav.contactInfo',
     return:
-      '["<a href="mailto:manavpatel5703@gmail.com">Gmail</a>", "<a rel="noopener" href="https://www.linkedin.com/in/justin-chi-64b12b37">LinkedIn</a>", "<a rel="noopener" href="https://github.com/vai0">GitHub</a>"]',
+      '["<a href="mailto:manavpatel5703@gmail.com">Gmail</a>", "<a rel="noopener" href="https://www.linkedin.com/in/manavpatel5703">LinkedIn</a>", "<a rel="noopener" href="https://github.com/Manav5703">GitHub</a>"]',
   },
   {
     input: 'Manav.resume',
