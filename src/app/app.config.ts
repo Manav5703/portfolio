5c9a0f7511dfeae9866b26c85656a97b086705ed
Manav <manavpatel5703.com>
2025-03-09 20:48:52 -0300

    fixed authentication for users

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { WebReqInterceptor } from './web-req.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withInterceptors([WebReqInterceptor])),
+  ]
 };
